365974cd10d0c4aadef1459feacfd2722a9fd7b1
Mrcoole7890 <Kendallcole7@gmail.com>
2015-11-25 18:05:15 -0400

    Cole's Demo Files
    2 Js files, one HTML5 file, loads of pictures

---
+++ project/animate.js
@@ -0,0 +1,10 @@
+var derp = function(){
+  $(".coolAni").toggle();
+  $(".coolAni").fadeIn(1000);
+  $("#rant").toggle();
+};
+var content = {
+  contentIndex: 0,
+  swapContent: function(item){
+  }
+}
